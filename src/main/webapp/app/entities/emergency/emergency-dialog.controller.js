c2362f120addb7360854cf875f42e5514d8b6df3
Sergio Castro <sergiojcr16@gmail.com>
2017-05-23 05:08:58 -0600

    Agrega la funcinalidad para notificar a los residentes cuando los oficiales vienen de camino

--- src/main/webapp/app/entities/emergency/emergency-dialog.controller.js
+++ src/main/webapp/app/entities/emergency/emergency-dialog.controller.js
@@ -12,10 +12,14 @@
         vm.isAuthenticated = Principal.isAuthenticated;
         vm.reportEmergency = save;
         $rootScope.active = "reportemergencyactive";
+        vm.enCamino = undefined;
        angular.element(document).ready(function(){
         $("#all").fadeIn("slow");
+        $("#normal").fadeIn("slow");
        })
-
+      var emergencyCode = $rootScope.companyId+""+$rootScope.companyUser.houseId;
+       WSEmergency.subscribeAttended(emergencyCode);
+       WSEmergency.receiveAttented(emergencyCode).then(null,null,emergencyAttended)
        function formatValidEmergency(){
        vm.emergency = {};
        vm.emergency.companyId = $rootScope.companyId;
@@ -41,15 +45,38 @@
            callback: function(result) {
                if (result) {
                formatValidEmergency();
+
                  WSEmergency.sendActivity(vm.emergency);
                         toastr["success"]("Se ha reportado la emergencia, enseguida será notificada a los oficiales");
+                        setTimeout(function(){
+                        toastr["info"]("Mantente en esta ventana para ser notificado(a) cuando los oficiales hayan escuchado tu alerta y vengan en camino.");
+                        },3000)
+
                         vm.emergency = undefined;
-                        $state.go('residentByHouse');
+//                        $state.go('residentByHouse');
                }
            }
        })
 
         }
+
+
+vm.closeIt = function(){
+
+$('#calma').fadeOut(300)
+setTimeout(function(){
+$('#normal').fadeIn(300)
+},300)
+}
+function emergencyAttended(emergency){
+
+
+$('#normal').fadeOut(300)
+
+setTimeout(function(){
+$('#calma').fadeIn(300)
+},300)
+}
 
         function onSaveSuccess (result) {
             $scope.$emit('aditumApp:emergencyUpdate', result);
@@ -60,7 +87,9 @@
         function onSaveError () {
             vm.isSaving = false;
         }
-
-
+$rootScope.$on('$stateChangeStart',
+function(event, toState, toParams, fromState, fromParams){
+  WSEmergency.unsubscribeAttended(emergencyCode);
+});
     }
 })();
--- src/main/webapp/app/entities/emergency/ws-emergency.service.js
+++ src/main/webapp/app/entities/emergency/ws-emergency.service.js
@@ -11,6 +11,8 @@
     function WSEmergency(StompManager,$rootScope) {
         var SUBSCRIBE_TRACKER_URL = '/topic/emergency/';
         var SEND_ACTIVITY_URL = '/topic/reportEmergency/';
+        var SUBSCRIBE_ATTEND_TRACKER_URL = '/topic/emergencyAttended/';
+        var SEND_ACTIVITY_ATTEND_URL = '/topic/attendEmergency/';
 
 
         var service = {
@@ -18,6 +20,11 @@
             sendActivity: sendActivity,
             subscribe: subscribe,
             unsubscribe: unsubscribe,
+            receiveAttented: receiveAttented,
+            sendActivityAttended: sendActivityAttended,
+            subscribeAttended: subscribeAttended,
+            unsubscribeAttended: unsubscribeAttended,
+
         };
 
         return service;
@@ -36,6 +43,22 @@
 
         function unsubscribe (companyId) {
             StompManager.unsubscribe(SUBSCRIBE_TRACKER_URL + companyId);
+        }
+
+        function receiveAttented (code) {
+                return StompManager.getListener(SUBSCRIBE_ATTEND_TRACKER_URL + code);
+        }
+
+        function sendActivityAttended(code,entity) {
+            StompManager.send(SEND_ACTIVITY_ATTEND_URL + code, entity);
+        }
+
+        function subscribeAttended(code) {
+            StompManager.subscribe(SUBSCRIBE_ATTEND_TRACKER_URL + code);
+        }
+
+        function unsubscribeAttended(code) {
+            StompManager.unsubscribe(SUBSCRIBE_ATTEND_TRACKER_URL + code);
         }
     }
 })();
