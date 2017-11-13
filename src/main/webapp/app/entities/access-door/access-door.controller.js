0d95b73008e1ca90ad44c567a61ae07244aeee3e
Sergio Castro <sergiojcr16@gmail.com>
2017-11-12 20:39:07 -0600

    Se agrega la opcion de multiples visitantes por placa

--- src/main/webapp/app/entities/access-door/access-door.controller.js
+++ src/main/webapp/app/entities/access-door/access-door.controller.js
@@ -532,26 +532,54 @@
 
                 });
                 return house;
-
             }
             vm.getVisitorByPlate = function(){
-
-            console.log("hola: "+vm.visitor_license_plate)
+            vm.visitorShowing = 0;
+            vm.visitorSelected = {}
+            vm.visitorsConsultedByPlate = [];
             if(vm.visitor_license_plate!=undefined || vm.visitor_license_plate!=""){
-                         console.log("ENTRE")
-                            angular.forEach(visitorsList, function(itemVisitor, index) {
-                                if (itemVisitor.licenseplate == vm.visitor_license_plate.toUpperCase() && itemVisitor.isinvited == 3 ) {
-                                    vm.visitor_name = itemVisitor.name;
-                                    vm.visitor_last_name = itemVisitor.lastname;
-                                    vm.visitor_second_last_name = itemVisitor.secondlastname;
-                                    vm.visitor_license_plate = itemVisitor.licenseplate;
-                                    vm.visitor_id_number = itemVisitor.identificationnumber;
-                                    setHouse(itemVisitor.houseId);
-                                }
-                            });
+                vm.visitorsConsultedByPlate = [];
+                angular.forEach(visitorsList, function(itemVisitor, index) {
+                    if (itemVisitor.licenseplate == vm.visitor_license_plate.toUpperCase() && itemVisitor.isinvited == 3 ) {
+                    var existe = 0;
+                    angular.forEach(vm.visitorsConsultedByPlate,function(visitor,index){
+                     if(visitor.identificationnumber==itemVisitor.identificationnumber){
+                     existe++;
+                     }
+                    })
+                    if(existe==0){
+                    vm.visitorsConsultedByPlate.push(itemVisitor)
+                    }
+                    }
+                });
+                if(vm.visitorsConsultedByPlate.length>0){
+                  vm.visitorsConsultedByPlate.reverse();
+                  vm.setVisitorConsulted(vm.visitorShowing)
+                }
               }
 
             }
+
+
+            vm.setVisitorConsulted = function(index){
+              vm.visitor_name = vm.visitorsConsultedByPlate[index].name;
+                vm.visitor_last_name = vm.visitorsConsultedByPlate[vm.visitorShowing].lastname;
+                vm.visitor_second_last_name = vm.visitorsConsultedByPlate[vm.visitorShowing].secondlastname;
+                vm.visitor_license_plate = vm.visitorsConsultedByPlate[vm.visitorShowing].licenseplate;
+                vm.visitor_id_number = vm.visitorsConsultedByPlate[vm.visitorShowing].identificationnumber;
+                setHouse(vm.visitorsConsultedByPlate[vm.visitorShowing].houseId);
+            }
+            vm.setVisitorSelected = function(){
+            if(vm.visitorSelected!=undefined){
+                vm.visitor_name = vm.visitorSelected.name;
+                vm.visitor_last_name = vm.visitorSelected.lastname;
+                vm.visitor_second_last_name = vm.visitorSelected.secondlastname;
+                vm.visitor_license_plate = vm.visitorSelected.licenseplate;
+                vm.visitor_id_number = vm.visitorSelected.identificationnumber;
+                setHouse(vm.visitorSelected.houseId);
+                }
+            }
+
             vm.capitalize = function(){
                 if (vm.visitor_license_plate != "") {
                         $("#license_plate").css("text-transform", "uppercase");
@@ -607,6 +635,7 @@
             }
             vm.searchVisitor = function() {
                 vm.show = 5;
+                vm.visitorsConsultedByPlate = [];
                 if(vm.id_vehicule == undefined){
                 $("#license_plate").css("text-transform", "none");
                 $("#license_plate").attr("placeholder", "Número placa (sin guiones)");
@@ -635,16 +664,7 @@
 
                 if (vm.id_vehicule == undefined || vm.id_vehicule == "") {} else {
                     vm.visitor_license_plate = vm.id_vehicule;
-                    angular.forEach(visitorsList, function(itemVisitor, index) {
-                        if (itemVisitor.licenseplate == vm.visitor_license_plate.toUpperCase() && itemVisitor.isinvited == 3) {
-                         vm.visitor_name = itemVisitor.name;
-                            vm.visitor_last_name = itemVisitor.lastname;
-                            vm.visitor_second_last_name = itemVisitor.secondlastname;
-                            vm.visitor_license_plate = itemVisitor.licenseplate;
-                            vm.visitor_id_number = itemVisitor.identificationnumber;
-                             setHouse(itemVisitor.houseId);
-                        }
-                    });
+                    vm.getVisitorByPlate();
                 }
             }
 
@@ -690,6 +710,7 @@
                  vm.isInsertingVisitor = false;
                  visitorsList.push(result);
                  toastr["success"]("Se registró la entrada del visitante correctamente.");
+                 loadVisitors();
              }
 
              function onSaveError () {
