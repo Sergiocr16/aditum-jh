1195d4e654b3c40dbd40efda85b745d52670f517
diegobari06 <diegobari06@hotmail.com>
2018-02-12 16:24:58 -0600

    dando estilo a la pestaña bienvenido de codelogin

--- src/main/webapp/app/entities/loginCode/insertLoginCode.controller.js
+++ src/main/webapp/app/entities/loginCode/insertLoginCode.controller.js
@@ -8,7 +8,11 @@
     InsertLoginCodeController.$inject = ['$rootScope', '$state','Principal', '$timeout', 'Auth','MultiCompany','House','$localStorage'];
 
     function InsertLoginCodeController ($rootScope, $state,Principal, $timeout, Auth,MultiCompany, House,$localStorage) {
-         angular.element(document).ready(function () {
+        var vm = this;
+        vm.loginCodeNotFound = 0;
+        $("#code_login_input").attr("placeholder", "Ingrese el código");
+
+        angular.element(document).ready(function () {
 
              $("#insertCode").fadeIn(700);
              setTimeout(function() {
@@ -26,20 +30,30 @@
 
          });
 
-        var vm = this;
 
-        vm.loginCodeNotFound = 0;
+        vm.changeStatus = function () {
+            vm.loginCodeNotFound = 0;
+            if(vm.loginCode == "" ){
+                $("#code_login_input").css("text-transform", "none");
+                $("#code_login_input").attr("placeholder", "Ingrese el código");
+            } else {
+                $("#code_login_input").css("text-transform", "uppercase");
+            }
 
+        }
         vm.validateLoginCode = function(){
             House.getByLoginCode({loginCode:vm.loginCode}).$promise.then(onSuccess, onError);
         }
          function onSuccess(data) {
-             $state.go('loginCodeWelcome',{loginCode:vm.loginCode});
+          $state.go('loginCodeWelcome',{loginCode:vm.loginCode});
         }
 
         vm.back = function(){
-
             $state.go('home');
+            $rootScope.menu = false;
+            $rootScope.companyId = undefined;
+            $rootScope.showLogin = true;
+            $rootScope.inicieSesion = false;
         }
           function onError() {
               vm.loginCodeNotFound = 1;
