8129a9a7e92a4af30e569a301a2fa5594e3efce8
Sergio Castro <sergiojcr16@gmail.com>
2018-02-19 15:43:02 -0600

    Se arregla el swipe

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -72,10 +72,10 @@
             swipe_det.sY = 0;
             swipe_det.eX = 0;
             swipe_det.eY = 0;
-            var min_x = 50; //min x swipe for horizontal swipe
-            var max_x = 40; //max x difference for vertical swipe
-            var min_y = 40; //min y swipe for vertical swipe
-            var max_y = 300; //max y difference for horizontal swipe
+          var min_x = 200; //min x swipe for horizontal swipe
+          var max_x = 40; //max x difference for vertical swipe
+          var min_y = 40; //min y swipe for vertical swipe
+          var max_y = 600; //max y difference for horizontal swipe
             var direc = "";
             var ele = document.getElementById(el);
             ele.addEventListener('touchstart', function(e) {
