7a5972c840b61e6b10ebe31b229601b9b877feb8
diegobari06 <diegobari06@hotmail.com>
2018-08-21 12:46:24 -0600

    Continuando regitrar area comun

--- src/main/webapp/app/entities/account-status/accountStatus.service.js
+++ src/main/webapp/app/entities/account-status/accountStatus.service.js
@@ -15,8 +15,8 @@
                 houseId: '@houseId',
                 initial_time:'@initial_time',
                 final_time: '@final_time',
-                final_time: '@resident_account',
-                final_time: '@today_time'
+                resident_account: '@resident_account',
+                today_time: '@today_time'
 
             },
             transformResponse: function (data) {
--- src/main/webapp/app/entities/egress/egress-dialog.controller.js
+++ src/main/webapp/app/entities/egress/egress-dialog.controller.js
@@ -1,16 +1,15 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('aditumApp')
         .controller('EgressDialogController', EgressDialogController);
 
-    EgressDialogController.$inject = ['CommonMethods','$timeout','$state', '$scope', '$stateParams','previousState', 'entity', 'Egress', 'Company','Principal','Proveedor','$rootScope','Banco','EgressCategory'];
+    EgressDialogController.$inject = ['CommonMethods', '$timeout', '$state', '$scope', '$stateParams', 'previousState', 'entity', 'Egress', 'Company', 'Principal', 'Proveedor', '$rootScope', 'Banco', 'EgressCategory'];
 
-    function EgressDialogController (CommonMethods,$timeout, $state, $scope, $stateParams, previousState,  entity, Egress, Company,Principal,Proveedor,$rootScope,Banco,EgressCategory) {
+    function EgressDialogController(CommonMethods, $timeout, $state, $scope, $stateParams, previousState, entity, Egress, Company, Principal, Proveedor, $rootScope, Banco, EgressCategory) {
         var vm = this;
-             $rootScope.active = "newEgress";
-
+        $rootScope.active = "newEgress";
 
 
         vm.isAuthenticated = Principal.isAuthenticated;
@@ -24,178 +23,183 @@
         vm.gastosOtros = [];
         CommonMethods.validateNumbers();
         CommonMethods.formatCurrencyInputs();
-          $(function() {
+        $(function () {
 
-            });
-
-
+        });
 
 
-        setTimeout(function(){
-        Proveedor.query({companyId: $rootScope.companyId}).$promise.then(onSuccessProveedores);
-        function onSuccessProveedores(data, headers) {
-              vm.proveedores = data;
+        setTimeout(function () {
+            Proveedor.query({companyId: $rootScope.companyId}).$promise.then(onSuccessProveedores);
 
-              Banco.query({companyId: $rootScope.companyId}).$promise.then(onSuccessBancos);
-                  function onSuccessBancos(data, headers) {
-                        vm.bancos = data;
+            function onSuccessProveedores(data, headers) {
+                vm.proveedores = data;
 
-                       EgressCategory.query({companyId: $rootScope.companyId}).$promise.then(onSuccessEgressCategories);
-                  }
+                Banco.query({companyId: $rootScope.companyId}).$promise.then(onSuccessBancos);
+
+                function onSuccessBancos(data, headers) {
+                    vm.bancos = data;
+
+                    EgressCategory.query({companyId: $rootScope.companyId}).$promise.then(onSuccessEgressCategories);
+                }
 
 
-        }},700)
-
-
-            function onSuccessEgressCategories(data, headers) {
-                 angular.forEach(data, function(value, key) {
-                       if(value.group=='Gastos fijos'){
-                        vm.gastosFijos.push(value)
-                        console.log(vm.gastosFijos);
-                       }
-                       if(value.group=='Gastos variables'){
-                        vm.gastosVariables.push(value)
-                       }
-
-                      if(value.group=='Otros gastos'){
-                       vm.gastosOtros.push(value)
-                      }
-
-                 })
-                 vm.egressCategories = data;
-                 setTimeout(function() {
-                              $("#loadingIcon").fadeOut(300);
-                  }, 400)
-                     setTimeout(function() {
-                         $("#new_egress_form").fadeIn('slow');
-                  },900 )
             }
+        }, 700)
 
 
-         vm.formatearNumero =function(nStr) {
+        function onSuccessEgressCategories(data, headers) {
+            angular.forEach(data, function (value, key) {
+                if (value.group == 'Gastos fijos') {
+                    vm.gastosFijos.push(value)
+                    console.log(vm.gastosFijos);
+                }
+                if (value.group == 'Gastos variables') {
+                    vm.gastosVariables.push(value)
+                }
+
+                if (value.group == 'Otros gastos') {
+                    vm.gastosOtros.push(value)
+                }
+
+            })
+            vm.egressCategories = data;
+            setTimeout(function () {
+                $("#loadingIcon").fadeOut(300);
+            }, 400)
+            setTimeout(function () {
+                $("#new_egress_form").fadeIn('slow');
+            }, 900)
+        }
+
+
+        vm.formatearNumero = function (nStr) {
 
             var x = nStr.split('.');
             var x1 = x[0];
             var x2 = x.length > 1 ? ',' + x[1] : '';
-             var rgx = /(\d+)(\d{3})/;
-             while (rgx.test(x1)) {
-                     x1 = x1.replace(rgx, '$1' + ',' + '$2');
-             }
-             vm.egress.total = x1 + x2;
-         }
+            var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1)) {
+                x1 = x1.replace(rgx, '$1' + ',' + '$2');
+            }
+            vm.egress.total = x1 + x2;
+        }
 
 
-          if(vm.egress.id != null){
+        if (vm.egress.id != null) {
 //          vm.egress.account = null;
 //          vm.egress.paymentMethod = null;
 //          vm.egress.paymentDate = null;
             vm.title = 'Reportar pago';
             vm.button = "Reportar";
             vm.picker3 = {
-                 datepickerOptions: {
-                     minDate:vm.egress.date,
-                     enableTime: false,
-                     showWeeks: false,
-                 }
-             }
-            Proveedor.get({id: vm.egress.proveedor},onSuccessProovedor)
+                datepickerOptions: {
+                    minDate: vm.egress.date,
+                    enableTime: false,
+                    showWeeks: false,
+                }
+            }
+            Proveedor.get({id: vm.egress.proveedor}, onSuccessProovedor)
+
             function onSuccessProovedor(proovedor, headers) {
-             vm.egress.empresa = proovedor.empresa;
+                vm.egress.empresa = proovedor.empresa;
 
             }
-                 if(vm.egress.folio == null || vm.egress.folio == 'undefined' ){
-                   vm.egress.folio = 'Sin Registrar'
-                  }
-                  if(vm.egress.billNumber == null || vm.egress.billNumber == 'undefined' || vm.egress.billNumber == '' ){
-                    vm.egress.billNumber = 'Sin Registrar'
-                   }
-        } else{
-          vm.title = "Capturar gasto";
-          vm.button = "Registrar";
+
+            if (vm.egress.folio == null || vm.egress.folio == 'undefined') {
+                vm.egress.folio = 'Sin Registrar'
+            }
+            if (vm.egress.billNumber == null || vm.egress.billNumber == 'undefined' || vm.egress.billNumber == '') {
+                vm.egress.billNumber = 'Sin Registrar'
+            }
+        } else {
+            vm.title = "Capturar gasto";
+            vm.button = "Registrar";
         }
-        $timeout(function (){
+        $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
-        function clear () {
+        function clear() {
             $uibModalInstance.dismiss('cancel');
         }
-         vm.confirmMessage =function(){
+
+        vm.confirmMessage = function () {
             if (vm.egress.id != null) {
-               confirmReportPayment();
+                confirmReportPayment();
 
-             } else {
-              confirmCreateEgress();
-             }
-         }
-
-
-        function confirmCreateEgress(){
-               bootbox.confirm({
-                     message: '<div class="text-center gray-font font-15"><h3 style="margin-bottom:30px;">¿Está seguro que desea registrar este egreso?</h3><h5 class="bold">Una vez registrada esta información no se podrá editar</h5></div>',
-                        buttons: {
-                            confirm: {
-                                label: 'Aceptar',
-                                className: 'btn-success'
-                            },
-                            cancel: {
-                                label: 'Cancelar',
-                                className: 'btn-danger'
-                            }
-                        },
-                        callback: function(result) {
-
-                            if (result) {
-                                    save()
-
-                            }else{
-                                vm.isSaving = false;
-
-                            }
-                        }
-                    });
+            } else {
+                confirmCreateEgress();
             }
-
-       function confirmReportPayment(){
-           bootbox.confirm({
-                 message: '<div class="text-center gray-font font-15"><h3 style="margin-bottom:30px;">¿Está seguro que desea reportar el pago de este egreso?</h3><h5 class="bold">Una vez registrada esta información no se podrá editar</h5></div>',
-                    buttons: {
-                        confirm: {
-                            label: 'Aceptar',
-                            className: 'btn-success'
-                        },
-                        cancel: {
-                            label: 'Cancelar',
-                            className: 'btn-danger'
-                        }
-                    },
-                    callback: function(result) {
-
-                        if (result) {
-                                save()
-
-                        }else{
-                            vm.isSaving = false;
-
-                        }
-                    }
-                });
         }
-        function save () {
+
+
+        function confirmCreateEgress() {
+            bootbox.confirm({
+                message: '<div class="text-center gray-font font-15"><h3 style="margin-bottom:30px;">¿Está seguro que desea registrar este egreso?</h3><h5 class="bold">Una vez registrada esta información no se podrá editar</h5></div>',
+                buttons: {
+                    confirm: {
+                        label: 'Aceptar',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'Cancelar',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function (result) {
+
+                    if (result) {
+                        save()
+
+                    } else {
+                        vm.isSaving = false;
+
+                    }
+                }
+            });
+        }
+
+        function confirmReportPayment() {
+            bootbox.confirm({
+                message: '<div class="text-center gray-font font-15"><h3 style="margin-bottom:30px;">¿Está seguro que desea reportar el pago de este egreso?</h3><h5 class="bold">Una vez registrada esta información no se podrá editar</h5></div>',
+                buttons: {
+                    confirm: {
+                        label: 'Aceptar',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'Cancelar',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function (result) {
+
+                    if (result) {
+                        save()
+
+                    } else {
+                        vm.isSaving = false;
+
+                    }
+                }
+            });
+        }
+
+        function save() {
             CommonMethods.waitingMessage();
             var currentTime = new Date(moment(new Date()).format("YYYY-MM-DD") + "T" + moment(new Date()).format("HH:mm:ss") + "-06:00").getTime();
             var expirationTime = new Date(vm.egress.expirationDate).getTime();
             if (currentTime <= expirationTime) {
                 vm.egress.state = 1;
-             } else {
-               vm.egress.state = 3;
-             }
-             if(vm.egress.paymentDate != null || vm.egress.paymentDate == 'undefined' ){
-                  vm.egress.state = 2;
-              }
+            } else {
+                vm.egress.state = 3;
+            }
+            if (vm.egress.paymentDate != null || vm.egress.paymentDate == 'undefined') {
+                vm.egress.state = 2;
+            }
             vm.isSaving = true;
             if (vm.egress.id != null) {
-                 Egress.update(vm.egress, onSaveReport, onSaveError);
+                Egress.update(vm.egress, onSaveReport, onSaveError);
 
             } else {
                 vm.egress.companyId = $rootScope.companyId;
@@ -205,7 +209,8 @@
                 Egress.save(vm.egress, onSaveSuccess, onSaveError);
             }
         }
-        function onSaveReport (result) {
+
+        function onSaveReport(result) {
             bootbox.hideAll();
             $scope.$emit('aditumApp:egressUpdate', result);
             $state.go('egress');
@@ -213,7 +218,7 @@
             vm.isSaving = false;
         }
 
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             bootbox.hideAll();
             $scope.$emit('aditumApp:egressUpdate', result);
             $state.go('egress');
@@ -221,32 +226,33 @@
             vm.isSaving = false;
         }
 
-        function onSaveError () {
-             bootbox.hideAll()
-         toastr["error"]("Un error inesperado ocurrió");
+        function onSaveError() {
+            bootbox.hideAll()
+            toastr["error"]("Un error inesperado ocurrió");
             vm.isSaving = false;
         }
-         vm.updatePicker = function() {
+
+        vm.updatePicker = function () {
             vm.picker1 = {
-                 datepickerOptions: {
-                     maxDate: vm.egress.expirationDate == undefined ? new Date() : vm.egress.expirationDate,
-                     enableTime: false,
-                     showWeeks: false,
-                 }
-             };
-             vm.picker2 = {
-                 datepickerOptions: {
-                     minDate:vm.egress.date,
-                     enableTime: false,
-                     showWeeks: false,
-                 }
-             }
+                datepickerOptions: {
+                    maxDate: vm.egress.expirationDate == undefined ? new Date() : vm.egress.expirationDate,
+                    enableTime: false,
+                    showWeeks: false,
+                }
+            };
+            vm.picker2 = {
+                datepickerOptions: {
+                    minDate: vm.egress.date,
+                    enableTime: false,
+                    showWeeks: false,
+                }
+            }
         }
         vm.datePickerOpenStatus.date = false;
         vm.datePickerOpenStatus.paymentDate = false;
         vm.datePickerOpenStatus.expirationDate = false;
 
-        function openCalendar (date) {
+        function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
         }
     }
