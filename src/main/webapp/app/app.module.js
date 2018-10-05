653670a4c303cbfad25dfc4b81afdbdf7f55f850
diegobari06 <diegobari06@hotmail.com>
2018-10-05 16:00:18 -0600

    Se agregan los serivios para los nuevos toasters y dialogs

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -72,58 +72,6 @@
             messagingSenderId: "720753236578"
         };
         firebase.initializeApp(config);
-        $('body').addClass("white-bg");
-
-        function detectswipe(el, func) {
-            var swipe_det = new Object();
-            swipe_det.sX = 0;
-            swipe_det.sY = 0;
-            swipe_det.eX = 0;
-            swipe_det.eY = 0;
-          var min_x = 200; //min x swipe for horizontal swipe
-          var max_x = 40; //max x difference for vertical swipe
-          var min_y = 40; //min y swipe for vertical swipe
-          var max_y = 600; //max y difference for horizontal swipe
-            var direc = "";
-            var ele = document.getElementById(el);
-            ele.addEventListener('touchstart', function(e) {
-                var t = e.touches[0];
-                swipe_det.sX = t.screenX;
-                swipe_det.sY = t.screenY;
-            }, false);
-            ele.addEventListener('touchmove', function(e) {
-                e.preventDefault();
-                var t = e.touches[0];
-                swipe_det.eX = t.screenX;
-                swipe_det.eY = t.screenY;
-            }, false);
-            ele.addEventListener('touchend', function(e) {
-                //horizontal detection
-                if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-                    if (swipe_det.eX > swipe_det.sX) direc = "r";
-                    else direc = "l";
-                }
-                //vertical detection
-                if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
-                    if (swipe_det.eY > swipe_det.sY) direc = "d";
-                    else direc = "u";
-                }
-
-                if (direc != "") {
-                    if (typeof func == 'function') func(el, direc);
-                }
-                direc = "";
-            }, false);
-        }
-
-        function swiping(el, d) {
-            if (d == 'r') {
-                history.back();
-            } else if (d == 'l') {
-                history.go(1);
-            }
-
-        }
 
         vm.navigated = false;
         vm.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
@@ -132,6 +80,8 @@
             }
         });
 
-        detectswipe('body', swiping);
+
+
+      vm.showContent = true;
     }
 })();
---
+++ src/main/webapp/app/components/util/modal-util.js
@@ -0,0 +1,124 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('aditumApp')
+        .factory('Modal', Modal);
+    Modal.$inject = ['$mdToast', '$mdDialog'];
+
+    function Modal($mdToast, $mdDialog) {
+        var service = {
+            toast: toast,
+            actionToast: actionToast,
+            dialog: dialog,
+            confirmDialog: confirmDialog,
+            confirmDialogCustom: confirmDialogCustom
+        };
+        var $scope = {};
+        var last = {
+            bottom: false,
+            top: true,
+            left: false,
+            right: true
+        };
+
+        $scope.toastPosition = angular.extend({}, last);
+
+        $scope.getToastPosition = function () {
+            sanitizePosition();
+
+            return Object.keys($scope.toastPosition)
+                .filter(function (pos) {
+                    return $scope.toastPosition[pos];
+                })
+                .join(' ');
+        };
+
+        function sanitizePosition() {
+            var current = $scope.toastPosition;
+
+            if (current.bottom && last.top) current.top = false;
+            if (current.top && last.bottom) current.bottom = false;
+            if (current.right && last.left) current.left = false;
+            if (current.left && last.right) current.right = false;
+
+            last = angular.extend({}, current);
+        }
+
+        var pinTo = $scope.getToastPosition();
+
+        return service;
+
+        function toast(text) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent(text)
+                    .position(pinTo)
+                    .hideDelay(3000)
+            );
+        }
+
+        function actionToast(text, actionText, action) {
+            var toast = $mdToast.simple()
+                .textContent(text)
+                .action(actionText)
+                .highlightAction(true)
+                .highlightClass('md-accent')// Accent is used by default, this just demonstrates the usage.
+                .position(pinTo);
+
+            $mdToast.show(toast).then(function (response) {
+                if (response == 'ok') {
+                    action()
+                }
+            });
+        };
+
+        function dialog(title, content, okText) {
+            var ev = new Event('build');
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title(title)
+                    .textContent(content)
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok(okText)
+                    .targetEvent(ev)
+            );
+        }
+
+        function confirmDialog(title, content, ok) {
+            var ev = new Event('build');
+            var confirm = $mdDialog.confirm()
+                .title(title)
+                .textContent(content)
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok('Aceptar')
+                .cancel('Cancelar');
+
+            $mdDialog.show(confirm).then(function () {
+                ok()
+            },function() {
+
+            } );
+        };
+        function confirmDialogCustom (title, content, okText, cancelText, ok) {
+            var ev = new Event('build');
+            var confirm = $mdDialog.confirm()
+                .title(title)
+                .textContent(content)
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok(okText)
+                .cancel(cancelText);
+            $mdDialog.show(confirm).then(function () {
+                ok()
+            }, function () {
+
+            });
+        };
+
+    }
+})
+();
