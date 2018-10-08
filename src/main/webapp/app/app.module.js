71e2a757a7565eb6b781fda998eb21458f334261
Sergio Castro <sergiojcr16@gmail.com>
2018-10-08 17:11:21 -0600

    Se modifica el menu para que tenga secciones, se modifica el seleccionar condominio

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -38,23 +38,7 @@
 
         stateHandler.initialize();
         translationHandler.initialize();
-        toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
+
         vm.isInLogin = $state.includes('home');
         vm.isInManual = $state.includes('manualResidente');
         vm.loadingDash = false;
