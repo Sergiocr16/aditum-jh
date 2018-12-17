b246832f1e6e48e801b960ec1038c3d443bccc7a
Sergio Castro <sergiojcr16@gmail.com>
2018-12-17 16:44:18 -0600

    Minor changes

--- src/main/webapp/app/entities/egress/egress-tabs.controller.js
+++ src/main/webapp/app/entities/egress/egress-tabs.controller.js
@@ -8,7 +8,36 @@
     EgressTabsController.$inject = [];
 
     function EgressTabsController() {
+        var vm = this;
+        vm.findBootstrapEnvironment = function () {
+            var envs = ['xs', 'sm', 'md', 'lg'];
 
+            var $el = $('<div>');
+            $el.appendTo($('body'));
+
+            for (var i = envs.length - 1; i >= 0; i--) {
+                var env = envs[i];
+
+                $el.addClass('hidden-' + env);
+                if ($el.is(':hidden')) {
+                    $el.remove();
+                    return env;
+                }
+            }
+        }
+        vm.isScreenSizeSmall = function () {
+            var envs = ['xs', 'sm', 'md'];
+            var e = 0;
+            for (var i = 0; i < envs.length; i++) {
+                if (envs[i] === vm.findBootstrapEnvironment()) {
+                    e++;
+                }
+            }
+            if (e > 0) {
+                return true;
+            }
+            return false;
+        }
 
     }
 })();
