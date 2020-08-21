9830928bdcba1479d8dad9eedf5a80e08dcfabc3
Sergio Castro <sergiojcr16@gmail.com>
2020-08-21 17:41:19 -0600

    Se permite ver las notas de los oficiales

--- src/main/webapp/app/entities/note/note.controller.js
+++ src/main/webapp/app/entities/note/note.controller.js
@@ -1,60 +1,215 @@
-(function() {
-    'use strict';
+(function () {
+        'use strict';
 
-    angular
-        .module('aditumApp')
-        .controller('NoteController', NoteController);
+        angular
+            .module('aditumApp')
+            .controller('NoteController', NoteController);
 
-    NoteController.$inject = ['$state', 'Note', 'ParseLinks', 'AlertService', 'paginationConstants', 'pagingParams'];
+        NoteController.$inject = ['Auth', '$state', '$scope', '$rootScope', 'CommonMethods', 'AccessDoor', 'Resident', 'House', 'Vehicule', 'Visitant', 'Note', 'AlertService', 'Emergency', 'Principal', '$filter', 'WSDeleteEntity', 'WSEmergency', 'WSHouse', 'WSResident', 'WSVehicle', 'WSNote', 'WSVisitor', 'PadronElectoral', 'Destinies', 'globalCompany', 'Modal', 'ParseLinks'];
 
-    function NoteController($state, Note, ParseLinks, AlertService, paginationConstants, pagingParams) {
+        function NoteController(Auth, $state, $scope, $rootScope, CommonMethods, AccessDoor, Resident, House, Vehicule, Visitant, Note, AlertService, Emergency, Principal, $filter, WSDeleteEntity, WSEmergency, WSHouse, WSResident, WSVehicle, WSNote, WSVisitor, PadronElectoral, Destinies, globalCompany, Modal, ParseLinks) {
+            var vm = this;
+            vm.houseSelectedNote = -1;
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-bottom-full-width",
+                "preventDuplicates": true,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "100000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "slideDown",
+                "hideMethod": "slideUp",
+            };
 
-        var vm = this;
+            House.getAllHousesClean({companyId: globalCompany.getId()}, function (data) {
+                vm.houses = data;
+            });
+            vm.clearSearchTerm = function () {
+                vm.searchTerm = '';
+            };
+            vm.searchTerm;
+            vm.typingSearchTerm = function (ev) {
+                ev.stopPropagation();
+            }
+            vm.loadingForNotification = false;
+            vm.noteCreatedBy = -1;
+            $rootScope.activeOn = "admin-notes";
+            $rootScope.noteCreatedBy = -1;
+            $scope.$on("$destroy", function () {
+                $rootScope.houseNoteNotification = undefined;
+            });
+            $rootScope.mainTitle = "Notas Actuales";
+            moment.locale('es');
+            vm.isReady = false;
+            vm.deletedStatusNote = 0;
+            vm.page = 0;
+            vm.links = {
+                last: 0
+            };
+            vm.itemsPerPage = 6;
+            vm.loadNotes = function () {
+                // Modal.showLoadingBar();
+                if(vm.deletedStatusNote==0){
+                    $rootScope.mainTitle = "Notas Actuales";
+                }else{
+                    $rootScope.mainTitle = "Notas Archivadas";
+                }
+                vm.isReady = false;
+                vm.page = 0;
+                vm.links = {
+                    last: 0
+                };
+                vm.notes = [];
+                vm.noteCreatedBy = vm.noteCreatedBy;
+                if (vm.houseSelectedNote == -1) {
+                    loadNotesByCompany();
+                } else {
+                    loadNotesByHouse();
+                }
+            }
+            vm.showArchivedNotes = function () {
+                vm.deletedStatusNote = 1;
+                $rootScope.mainTitle = "Notas Archivadas";
+                vm.loadNotes();
+            }
+            vm.showActualNotes = function () {
+                vm.deletedStatusNote = 0;
+                $rootScope.mainTitle = "Notas";
+                vm.loadNotes();
+            }
 
-        vm.loadPage = loadPage;
-        vm.predicate = pagingParams.predicate;
-        vm.reverse = pagingParams.ascending;
-        vm.transition = transition;
-        vm.itemsPerPage = paginationConstants.itemsPerPage;
+            function loadNotesByCompany() {
+                Note.findAllByCompanyAndDeleted({
+                    page: vm.page,
+                    size: vm.itemsPerPage,
+                    sort: sortNotes(),
+                    companyId: globalCompany.getId(),
+                    deleted: vm.deletedStatusNote,
+                    status: vm.noteCreatedBy
+                }, onSuccessNotes, onError);
+            }
 
-        loadAll();
 
-        function loadAll () {
-            Note.query({
-                page: pagingParams.page - 1,
-                size: vm.itemsPerPage,
-                sort: sort()
-            }, onSuccess, onError);
-            function sort() {
-                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
-                if (vm.predicate !== 'id') {
-                    result.push('id');
+            function loadNotesByHouse() {
+                Note.findAllByHouseAndDeleted({
+                    page: vm.page,
+                    size: vm.itemsPerPage,
+                    sort: sortNotes(),
+                    houseId: vm.houseSelectedNote,
+                    deleted: vm.deletedStatusNote,
+                    status: vm.noteCreatedBy
+                }, onSuccessNotes, onError);
+            }
+
+            function sortNotes() {
+                var result = [];
+                if (vm.predicate !== 'creationdate') {
+                    result.push('creationdate,desc');
                 }
                 return result;
             }
-            function onSuccess(data, headers) {
+
+            function onSuccessNotes(data, headers) {
                 vm.links = ParseLinks.parse(headers('link'));
                 vm.totalItems = headers('X-Total-Count');
-                vm.queryCount = vm.totalItems;
-                vm.notes = data;
-                vm.page = pagingParams.page;
+                console.log(data)
+                for (var i = 0; i < data.length; i++) {
+                    data[i].sinceDate = moment(data[i].creationdate).fromNow();
+                    vm.notes.push(data[i]);
+                }
+                Modal.hideLoadingBar();
+                vm.isReady = true;
             }
-            function onError(error) {
-                AlertService.error(error.data.message);
+
+            function onError() {
+
             }
-        }
 
-        function loadPage(page) {
-            vm.page = page;
-            vm.transition();
-        }
+            vm.loadPageNotes = function (page) {
+                vm.page = page;
+                if (vm.houseSelectedNote === -1) {
+                    loadNotesByCompany();
+                } else {
+                    loadNotesByHouse();
+                }
+            };
 
-        function transition() {
-            $state.transitionTo($state.$current, {
-                page: vm.page,
-                sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
-                search: vm.currentSearch
-            });
+            vm.delete = function (note) {
+                vm.note = note;
+                Modal.confirmDialog("¿Está seguro que desea archivar la nota?", "", function () {
+                    Note.delete({
+                        id: note.id
+                    }, onSuccessDelete, OnError);
+                })
+
+            }
+
+            function onSuccessDelete(result) {
+                CommonMethods.deleteFromArray(vm.note, $rootScope.notes);
+                Modal.actionToastGiant("Se archivo la nota correctamente", "Deshacer", function () {
+                    Note.restore({id: vm.note.id}, function () {
+                        vm.note.deleted = 0;
+                        if($rootScope.deletedStatusNote==0){
+                            vm.notes.push(vm.note);
+                        }else{
+                            CommonMethods.deleteFromArray(vm.note, vm.notes);
+                        }
+                    })
+                })
+            }
+
+            vm.restore = function (note) {
+                Modal.confirmDialog("¿Está seguro que desea restaurar  la nota?", "", function () {
+                    Note.restore({id: note.id}, function () {
+                        CommonMethods.deleteFromArray(note, $rootScope.notes);
+                        Modal.actionToastGiant("Se restauro la nota correctamente", "Deshacer", function () {
+                            Note.delete({
+                                id: note.id
+                            }, function(){
+                                if(vm.deletedStatusNote==1){
+                                    $rootScope.notes.push(note);
+                                }else{
+                                    CommonMethods.deleteFromArray(note, $rootScope.notes);
+                                }
+                            }, OnError);
+                        })
+                    });
+                })
+            };
+            function OnError(result) {
+                Modal.toastGiant("Ocurrio un error eliminando la nota");
+            }
+            $scope.$watch(function () {
+                return $rootScope.houseNoteNotification;
+            }, function () {
+                if ($rootScope.houseNoteNotification != undefined) {
+                    vm.checkNoteNotification();
+                }
+            }, true);
+
+            vm.checkNoteNotification = function(){
+                if(vm.houseNoteNotification==undefined){
+                    vm.houseSelectedNote = -1;
+                    vm.noteCreatedBy = -1;
+                }else{
+                    vm.houseSelectedNote = vm.houseNoteNotification;
+                    vm.noteCreatedBy = 1;
+                }
+                vm.deletedStatusNote = 0;
+                $rootScope.notes = [];
+                vm.loadNotes();
+            };
+            if($rootScope.houseNoteNotification==undefined){
+                vm.loadNotes();
+            }
         }
     }
-})();
+
+)();
--- src/main/webapp/app/entities/note/note.state.js
+++ src/main/webapp/app/entities/note/note.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -9,21 +9,84 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-
-        .state('noteNew', {
-            parent: 'entity',
-            url: '/home-service/new',
-            data: {
-                authorities: ['ROLE_USER','ROLE_OWNER']
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/note/note-dialog.html',
-                    controller: 'NoteDialogController',
-                    controllerAs: 'vm'
-                }
-            },
-        });
+            .state('admin-notes', {
+                url: '/admin-notes',
+                parent: 'entity',
+                data: {
+                    authorities: ['ROLE_MANAGER','ROLE_JD']
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/note/notes.html',
+                        controller: 'NoteController',
+                        controllerAs: 'vm',
+                    }
+                },
+            })
+            .state('admin-notes.new', {
+                parent: 'admin-notes',
+                url: '/new',
+                data: {
+                    authorities: ['ROLE_MANAGER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/note/note-dialog-admin.html',
+                        controller: 'NoteDialogAdmin',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'md',
+                        resolve: {
+                            entity: function () {
+                                return {};
+                            }
+                        }
+                    }).result.then(function () {
+                        $state.go('admin-notes', null, {reload: 'admin-notes'});
+                    }, function () {
+                        $state.go('admin-notes');
+                    });
+                }]
+            })
+            .state('admin-notes.edit', {
+                parent: 'admin-notes',
+                url: '/{id}/edit',
+                data: {
+                    authorities: ['ROLE_MANAGER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/note/note-dialog-admin.html',
+                        controller: 'NoteDialogAdmin',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'md',
+                        resolve: {
+                            entity: ['Note', function (Note) {
+                                return Note.get({id: $stateParams.id}).$promise;
+                            }]
+                        }
+                    }).result.then(function () {
+                        $state.go('access-door.notes', null, {reload: 'access-door.notes'});
+                    }, function () {
+                        $state.go('^');
+                    });
+                }]
+            })
+            .state('noteNew', {
+                parent: 'entity',
+                url: '/home-service/new',
+                data: {
+                    authorities: ['ROLE_USER', 'ROLE_OWNER']
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/note/note-dialog.html',
+                        controller: 'NoteDialogController',
+                        controllerAs: 'vm'
+                    }
+                },
+            });
     }
 
 })();
